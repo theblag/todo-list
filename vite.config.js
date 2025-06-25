a4196291f9cd39703e64a1afcee94ce1f374b37a
Aditya A <adityaarun2006@gmail.com>
2025-06-25 16:51:54 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/todolist/', // 👈 change this to match your GitHub repo name
   plugins: [react()],
 })
